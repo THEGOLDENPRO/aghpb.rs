c406319d5a58ee1271476325303e809ce513c21f
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-08-01 18:12:17 +0100

    big improvements and added some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,43 @@
+//! AGHPB API wrapper for 🦀Rust.
+//!
+//! Copyright (c) 2023-present Goldy
+//! 
+//! -------------
+//! 
+//! # Example
+//! How to retrieve a random anime girl holding a programming book in Rust.
+//! ```rust
+//! use std::error::Error;
+//! 
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn Error>> {
+//!     let book = aghpb::random(None).await?;
+//! 
+//!     println!("Name: {}", book.name);
+//!     println!("Category: {}", book.category);
+//! 
+//!     book.image.save("./anime_girl.png")?;
+//! 
+//!     Ok(())
+//! }
+//! ```
+
 use reqwest::Response;
 use std::error::Error;
+use image::DynamicImage;
 
 const API_URL: &str = "https://api.devgoldy.xyz/aghpb";
 
-pub async fn random(category: Option<String>) -> Result<Response, Box<dyn Error>> {
+pub struct Book {
+    pub name: String,
+    pub category: String,
+    pub image: DynamicImage,
+}
+
+pub async fn random(category: Option<String>) -> Result<Book, Box<dyn Error>> {
+    //! Asynchronously grabs a random anime girl holding a programming book.
+    //! 
+    //! Uses the ``/v1/random`` endpoint.
     let mut url: String = API_URL.to_owned() + "/v1/random";
 
     let category: String = category.unwrap_or("".to_string());
@@ -13,6 +47,13 @@ pub async fn random(category: Option<String>) -> Result<Response, Box<dyn Error>
     }
 
     let res: Response = reqwest::get(url).await?;
+    let headers: &reqwest::header::HeaderMap = res.headers();
 
-    Ok(res)
+    let book = Book {
+        name: String::from(headers.get("book-name").unwrap().to_str().unwrap()),
+        category: String::from(headers.get("book-category").unwrap().to_str().unwrap()),
+        image: image::load_from_memory(&(res.bytes().await?))?,
+    };
+
+    Ok(book)
 }
